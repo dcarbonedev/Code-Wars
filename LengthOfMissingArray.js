4192a2c1caff42707634b7c2664cde6c5b2dc6d3
Daniel <dan9221@msn.com>
2021-11-24 10:40:07 -0800

    modified LengthOfMissingArray.js

--- LengthOfMissingArray.js
+++ LengthOfMissingArray.js
@@ -15,3 +15,11 @@
 // When an array in the array is null or empty, the method should return 0 too!
 // There will always be a missing element and its length will be always between the given arrays.
 
+function getLengthOfMissingArray(arr) {
+  if(!arr || arr.reduce((a,c) => !c ? a + 1 : a, 0) > 0) return 0;
+  arr.sort((a,b) => a.length - b.length);
+  if(arr.length === 0 || arr[0].length === 0) return 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i+1].length - arr[i].length > 1) return arr[i].length+1;
+  }
+}
