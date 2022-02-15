87a37e6394281f9757f2d9d0b2228fbd5777fa52
Daniel <dan9221@msn.com>
2022-02-15 11:28:09 -0800

    modified BasicMathematicalOperations.js

--- BasicMathematicalOperations.js
+++ BasicMathematicalOperations.js
@@ -13,3 +13,19 @@
 // ('*', 5, 5) --> 25
 // ('/', 49, 7) --> 7
 
+function basicOp(operation, value1, value2) {
+    switch(operation){
+        case '+':
+        return value1 + value2;
+        break;
+        case '-':
+        return value1 - value2;
+        break;
+        case '/':
+        return value1 / value2;
+        break;
+        case '*':
+        return value1 * value2;
+        break;
+    }
+}
