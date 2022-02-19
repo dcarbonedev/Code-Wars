b130d0a76d7fd670827062de3b6229436e3e685a
Daniel <dan9221@msn.com>
2022-02-19 11:17:44 -0800

    modified MostDigits.js

--- MostDigits.js
+++ MostDigits.js
@@ -6,3 +6,7 @@
 // If two numbers in the argument array have the same number of digits, return the 
 // first one in the array.
 
+function findLongest(array){
+    let lengths = array.map(e => e.toString().length);
+    return array[lengths.indexOf(Math.max(...lengths))];
+}
