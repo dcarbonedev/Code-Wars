eb1df7f68ebfb11f0580f6c526dc119d51904010
Daniel <dan9221@msn.com>
2022-02-21 11:11:17 -0800

    added ReverseNumber.js

---
+++ ReverseNumber.js
@@ -0,0 +1,24 @@
+// 7 kyu
+// Reverse a Number
+
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the 
+//     answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative 
+// when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+    let arr = n.toString().split('');
+    if(n < 0) {
+      arr.shift();
+      arr.reverse().unshift('-');
+      return Number(arr.join(''));
+    } else {
+      return Number(arr.reverse().join(''));
+    }
+}
