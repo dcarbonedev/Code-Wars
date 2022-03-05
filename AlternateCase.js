c3b1d6e5d1b5c6225852a9dd194d7a0857cc3381
Daniel <dan9221@msn.com>
2022-03-05 10:50:14 -0800

    modified AlternateCase.js

--- AlternateCase.js
+++ AlternateCase.js
@@ -4,3 +4,4 @@
 // Write function alternateCase which switch every letter in string from upper to lower 
 // and from lower to upper. E.g: Hello World -> hELLO wORLD
 
+let alternateCase = s => [...s].map(e => e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()).join('');
