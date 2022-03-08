b4c306c8ccf0fa91b779f8b5df21a0d8df6fca3d
Daniel <dan9221@msn.com>
2022-03-08 10:39:42 -0800

    modified AnagramDetection.js

--- AnagramDetection.js
+++ AnagramDetection.js
@@ -15,5 +15,7 @@
 // "Buckethead" is an anagram of "DeathCubeK"
 
 // write the function isAnagram
-
+var isAnagram = function(test, original) {
+    return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('');
+};
   
