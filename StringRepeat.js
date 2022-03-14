b2540abb78e56f51ea75a2d1e343544419ea27b2
Daniel <dan9221@msn.com>
2022-03-14 10:23:08 -0700

    added StringRepeat.js

---
+++ StringRepeat.js
@@ -0,0 +1,16 @@
+// 8 kyu
+// String repeat
+
+// Write a function called repeatStr which repeats the given string string exactly n 
+// times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+    result = '';
+    for(let i = 0; i < n; i++) {
+        result += s;
+    }
+    return result;
+}
