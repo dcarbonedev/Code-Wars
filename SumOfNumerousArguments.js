34b616827d8fe5af3ab94b94d48e201e0ce7b910
Daniel <dan9221@msn.com>
2022-03-14 10:22:27 -0700

    added SumOfNumerousArguments.js

---
+++ SumOfNumerousArguments.js
@@ -0,0 +1,22 @@
+// 7 kyu
+// Sum of numerous arguments
+
+// After calling the function findSum() with any number of non-negative integer 
+// arguments, it should return the sum of all those arguments. If no arguments are given, 
+// the function should return 0, if negative arguments are given, it should return -1.
+
+// Example
+// findSum(1,2,3,4); // returns 10 
+// findSum(1,-2);    // returns -1 
+// findSum();        // returns 0 
+// Hint: research the arguments object on google or read Chapter 4 from Eloquent Javascript
+
+function findSum(...arr){
+    if(arr.some( e => e < 0)) {
+        return -1;
+    }
+    if(arr.length === 0) {
+        return 0;
+    }
+    return arr.reduce( (acc, cur) => acc + cur, 0);
+}
