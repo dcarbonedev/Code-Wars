498fd4bdb1c036d347e007290fb117baff0604a2
Daniel <dan9221@msn.com>
2022-03-17 10:26:54 -0700

    added SimpleMultiplication.js

---
+++ SimpleMultiplication.js
@@ -0,0 +1,8 @@
+// 8 kyu
+// Simple multiplication
+
+// This kata is about multiplying a given number by eight if it is an even number and 
+// by nine otherwise.
+
+let simpleMultiplication = n => n % 2 ? n * 9 : n * 8;
+
