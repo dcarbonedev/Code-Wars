3f3998eb96b69151a56f4f14af0d7712e57b8605
Daniel <dan9221@msn.com>
2022-04-25 12:54:14 -0700

    4-25-22 solutions

---
+++ 4-25-22/NumberToReversedArray.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Convert number to reversed array of digits
+
+// Given a random non-negative number, you have to return the digits of this number 
+// within an array in reverse order.
+
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+let digitize = n => String(n).split('').reverse().map(e => parseInt(e, 10));
