11c2d39b5a83c001c270a906bffaf7479f7bcd9e
Daniel <dan9221@msn.com>
2022-04-24 18:38:21 -0700

    4-24-22 solutions

---
+++ 4-24-22/TwoToOne.js
@@ -0,0 +1,16 @@
+// 7 kyu
+// Two to One
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted 
+// string, the longest possible, containing distinct letters - each taken only once - 
+// coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+let longest = (s1, s2) => Array.from(new Set([...s1].concat([...s2]))).sort().join('');
