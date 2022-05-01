f055b992aaa4e4cbeffbb773081a439744f251fd
Daniel <dan9221@msn.com>
2022-05-01 14:23:46 -0700

    more 5-1-22 solutions

---
+++ 5-1-22/BitCounting.js
@@ -0,0 +1,18 @@
+// 6 kyu
+// Bit Counting
+
+// Write a function that takes an integer as input, and returns the number of bits that 
+// are equal to one in the binary representation of that number. You can guarantee that 
+// input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should 
+// return 5 in this case
+
+var countBits = function(n) {
+    let binary = [];
+    while (n > 0) {
+      binary.push(n % 2);
+      n = Math.floor(n / 2);
+    }
+    return binary.filter(Boolean).length;
+};
