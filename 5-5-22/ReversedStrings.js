4495d3fbd876cf79ae81b009207e172a8ce71bc0
Daniel <dan9221@msn.com>
2022-05-05 08:35:43 -0700

    5-5-22 solutions

---
+++ 5-5-22/ReversedStrings.js
@@ -0,0 +1,9 @@
+// 8 kyu
+// Reversed Strings
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+let solution = s => s.split('').reverse().join('');
