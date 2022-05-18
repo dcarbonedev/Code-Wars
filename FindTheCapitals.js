8c25dee7b1b764ff2c5042a948159734df68793b
Daniel <dan9221@msn.com>
2022-05-18 08:56:49 -0700

    added FindTheCapitals.js

---
+++ FindTheCapitals.js
@@ -0,0 +1,20 @@
+// 7 kyu
+// Find the capitals
+
+// Instructions
+// Write a function that takes a single string (word) as argument. The function 
+// must return an ordered list containing the indexes of all capital letters in 
+// the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+var capitals = function (word) {
+    let result = [];
+      word.split('').forEach((e,i) => {
+      if(e === e.toUpperCase()) {
+        result.push(i);
+      }
+    });
+    return result;
+};
