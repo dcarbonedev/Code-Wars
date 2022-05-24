b6062ec7d568181e452716f98ce9ff3612249a64
Daniel <dan9221@msn.com>
2022-05-24 08:45:11 -0700

    modified LargestPairInSumArr.js

--- LargestPairInSumArr.js
+++ LargestPairInSumArr.js
@@ -9,3 +9,7 @@
 // [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 // Input sequence contains minimum two elements and every element is an integer.
 
+function largestPairSum (numbers) {
+    numbers.sort((a,b) => a-b).reverse();
+    return numbers[0] + numbers[1];
+}
