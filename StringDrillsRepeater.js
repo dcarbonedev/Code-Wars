d0238c10f5ea30058f2cda83706d018a09b67b66
Daniel <dan9221@msn.com>
2022-05-28 12:24:24 -0700

    added StringDrillsRepeater.js

---
+++ StringDrillsRepeater.js
@@ -0,0 +1,16 @@
+// 7 kyu
+// Thinkful - String Drills: Repeater
+
+// Write a function named repeater() that takes two arguments (a string and a number), 
+// and returns a new string where the input string is repeated that many times.
+
+// Example: (Input1, Input2 --> Output)
+// "a", 5 --> "aaaaa"
+
+function repeater(string, n){
+    let originalString = string;
+    for(let i = 1; i < n; i++) {
+      string += originalString;
+    }
+    return string;
+}
