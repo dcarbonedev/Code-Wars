7ea8ee036eb6501bbb1f86520bfed2489ac4df70
Daniel <dan9221@msn.com>
2022-05-29 11:57:30 -0700

    added PredictYourAge.js

---
+++ PredictYourAge.js
@@ -0,0 +1,15 @@
+// 7 kyu
+// Predict your age!
+
+// My grandfather always predicted how old people would get, and right before he passed 
+// away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+
+let predictAge = (...ages) => Math.floor(Math.sqrt(ages.reduce((a,c) => a + (c*c),0))/2);
