3a94493c2d199040570e385f0e172f4957878466
Daniel <dan9221@msn.com>
2022-06-02 09:07:32 -0700

    added Factorial2.js

---
+++ Factorial2.js
@@ -0,0 +1,14 @@
+// 7 kyu
+// Factorial
+
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+function factorial(n) {
+    let result = 1;
+    for(let i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+}
