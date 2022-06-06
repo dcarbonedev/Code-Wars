d9c858f4eba065c3d8015d9520297849660a0f77
Daniel <dan9221@msn.com>
2022-06-06 11:58:17 -0700

    modified CheckForPrime.js

--- CheckForPrime.js
+++ CheckForPrime.js
@@ -13,3 +13,11 @@
 // 11 --> true
 // 12 --> false
 
+function isPrime(n) {
+  if (n < 2) return false
+  
+  for(let i = 2; i < n/2; i++) {
+    if(n % i === 0) return false; 
+  }
+  return true;
+}
