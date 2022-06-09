f5d078602d199a7d881dd9ec6fa741a2b603a687
Daniel <dan9221@msn.com>
2022-06-09 11:09:44 -0700

    added BreakCamelCase.js

---
+++ BreakCamelCase.js
@@ -0,0 +1,20 @@
+// 6 kyu
+// Break camelCase
+
+// Complete the solution so that the function will break up camel casing, using a space 
+// between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+    let result = '';
+    for(let i = 0; i < string.length; i++) {
+      if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) result += ' ';
+      result += string[i];
+    }
+    return result;
+}
+  
