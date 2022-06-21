e79c78313a9aae0b26b970b0779e826135d880c3
Daniel <dan9221@msn.com>
2022-06-21 09:38:27 -0700

    modified Switcheroo.js

--- Switcheroo.js
+++ Switcheroo.js
@@ -8,3 +8,14 @@
 // 'acb' --> 'bca'
 // 'aabacbaa' --> 'bbabcabb'
 
+function switcheroo(x){
+  let arr = [...x];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 'a') {
+      arr[i] = 'b';
+    } else if(arr[i] === 'b') {
+      arr[i] = 'a';
+    }
+  }
+  return arr.join('');
+}
