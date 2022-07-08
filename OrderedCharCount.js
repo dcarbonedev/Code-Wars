9e63c7fb3a0c8038bea12e81ce50e680332f77d8
Daniel <dan9221@msn.com>
2022-07-08 13:17:59 -0700

    modified OrderedCharCount.js

--- OrderedCharCount.js
+++ OrderedCharCount.js
@@ -6,3 +6,12 @@
 
 // orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 
+var orderedCount = function (text) {
+  let result = [];
+  let letters = [...text];
+  while(letters.length > 0) {
+    result.push([letters[0], letters.reduce((a,c) => c === letters[0] ? a + 1 : a, 0)]);
+    letters = letters.filter(e => e !== letters[0]);
+  }
+  return result;
+}
