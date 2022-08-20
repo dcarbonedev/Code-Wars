a2182f241de1fc3158627d418b2583174d567264
Daniel <dan9221@msn.com>
2022-08-19 21:55:26 -0400

    modified EvenNumsInArray.js

--- EvenNumsInArray.js
+++ EvenNumsInArray.js
@@ -8,3 +8,7 @@
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
+function evenNumbers(array, number) {
+    let evens = array.filter(e => !(e%2));
+    return evens.slice(evens.length - number);
+}
