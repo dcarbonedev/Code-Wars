380c54201158c702739cc168dfbf70a38646dc68
Daniel <dan9221@msn.com>
2022-08-28 11:09:31 -0700

    modified WhichAreIn.js

--- WhichAreIn.js
+++ WhichAreIn.js
@@ -21,3 +21,12 @@
 // In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
 // Beware: r must be without duplicates.
 
+function inArray(array1,array2) {
+  let result = [];
+  for(let i = 0; i < array1.length; i++) {
+    for(let j = 0; j < array2.length; j++) {
+      if(array2[j].includes(array1[i])) result.push(array1[i]);
+    }
+  }
+  return Array.from(new Set(result.sort()));
+}
