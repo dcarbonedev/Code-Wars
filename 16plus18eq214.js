68a3fd25ccc92c8d5ee1c20019fc98e0b835358c
Daniel <dan9221@msn.com>
2022-09-01 11:16:08 -0700

    added 16plus18eq214.js

---
+++ 16plus18eq214.js
@@ -0,0 +1,12 @@
+// 16+18=214
+
+// For this kata you will have to forget how to add two numbers.
+
+// It can be best explained using the following meme:
+
+// Dayane Rivas adding up a sum while competing in the Guatemalan television show "Combate" in May 2016
+
+// In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
+
+// You may assume both integers are positive integers.
+
