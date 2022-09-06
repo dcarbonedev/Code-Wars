73a303d268808389bb4df6ee4a6b2bee1feec973
Daniel <dan9221@msn.com>
2022-09-06 11:23:06 -0700

    added FramedReflection.js

---
+++ FramedReflection.js
@@ -0,0 +1,11 @@
+// Framed Reflection
+
+// You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame. Example:
+
+// 'Hello World'
+
+// would give:
+
+
+// Words in your solution should be left-aligned.
+
