3411c9c9c534c71839596458df5d7dfdb8e2b44e
Daniel <dan9221@msn.com>
2022-09-09 11:26:46 -0700

    modified SumOddCubedNums.js

--- SumOddCubedNums.js
+++ SumOddCubedNums.js
@@ -2,3 +2,8 @@
 
 // Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 
+function cubeOdd(arr) {
+  if(arr.filter(e => Number.isInteger(e)).length === arr.length) {
+    return arr.reduce((a,c) => c % 2 ? a + c**3 : a ,0);
+  }
+}  
