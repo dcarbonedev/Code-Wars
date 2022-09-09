ac7ef22dfde6fe74da40fafe4166669fa1f0aa48
Daniel <dan9221@msn.com>
2022-09-09 11:26:32 -0700

    added SumOddCubedNums.js

---
+++ SumOddCubedNums.js
@@ -0,0 +1,4 @@
+// Sum of Odd Cubed Numbers
+
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+
