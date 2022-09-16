683f3813925a52a7363308060e71d46d364a082d
Daniel <dan9221@msn.com>
2022-09-16 09:01:51 -0700

    modified SimplePigLatin.js

--- SimplePigLatin.js
+++ SimplePigLatin.js
@@ -6,3 +6,16 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
+function pigIt(str){
+  let words = str.split(' ');  
+  for(let i = 0; i < words.length; i++) {
+    if(words[i] !== '!' && words[i] !== '?') {
+      let letters = words[i].split('');
+      letters.push(letters[0]);
+      letters.shift();
+      letters.push('ay');
+      words[i] = letters.join('');
+    }
+  }
+  return words.join(' ');
+}
