c5fe291ac70a51f40426156dd66768e70f5c0a84
Daniel <dan9221@msn.com>
2022-09-17 11:19:47 -0700

    modified Vaporcode.js

--- Vaporcode.js
+++ Vaporcode.js
@@ -10,3 +10,4 @@
 // "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 // "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 
+const vaporcode = s => [...s.split(' ').join('').toUpperCase()].map(e => e + '  ').join('').trim();
