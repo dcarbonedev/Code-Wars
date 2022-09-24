66b94be306983f65a1ad6821d6775ffcf41f4e90
Daniel <dan9221@msn.com>
2022-09-24 10:30:01 -0700

    modified TwistedSum.js

--- TwistedSum.js
+++ TwistedSum.js
@@ -22,3 +22,16 @@ function twistedSum(n) {
   return [...str].reduce((a,c) => a + +c, 0);
 }
 
+// Using just math
+function twistedSum(n) {
+  let sum = 0;
+  for(let i = 1; i <= n; i++) {
+    let num = i;
+    while(num > 0) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
+    }
+  }
+  return sum;
+}
+
