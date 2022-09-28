39f518acd1b562bfc9213c532068661bd3ad5df3
Daniel <dan9221@msn.com>
2022-09-28 12:14:42 -0700

    added AlphaAddition.js

---
+++ AlphaAddition.js
@@ -0,0 +1,23 @@
+// Alphabetical Addition
+
+// Your task is to add up letters to one letter.
+
+// The function will be given a variable amount of arguments, each one being a letter to add.
+
+// Notes:
+// Letters will always be lowercase.
+// Letters can overflow (see second to last example of the description)
+// If no letters are given, the function should return 'z'
+// Examples:
+// addLetters('a', 'b', 'c') = 'f'
+// addLetters('a', 'b') = 'c'
+// addLetters('z') = 'z'
+// addLetters('z', 'a') = 'a'
+// addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+// addLetters() = 'z'
+
+function addLetters(...letters) {
+    let l = 'abcdefghijklmnopqrstuvwxyz';
+    let num = letters.reduce((a,c) => a + l.indexOf(c) + 1, 0) % 26;
+    return !num ? 'z' : l[num-1];
+}
