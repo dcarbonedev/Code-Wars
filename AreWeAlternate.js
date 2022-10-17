92944d6ad04db357e7443c6b10f1d58c19ef8c7a
Daniel <dan9221@msn.com>
2022-10-17 12:40:28 -0700

    updated AreWeAlternate.js

--- AreWeAlternate.js
+++ AreWeAlternate.js
@@ -10,3 +10,14 @@
 // // true
 // Arguments consist of only lowercase letters.
 
+function isAlt(word) {
+  let cons = 'bcdfghjklmnpqrstvwxyz';
+  let vowels = 'aeiou';
+  for(let i = 1; i < word.length; i++) {
+    if( cons.includes(word[i-1]) && cons.includes(word[i]) ||
+        vowels.includes(word[i-1]) && vowels.includes(word[i]) ) {
+      return false;
+    }
+  }
+  return true;
+}
