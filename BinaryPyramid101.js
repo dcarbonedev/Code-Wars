6338e9f79015ae9b268f90527c79ed95511b9801
Daniel <dan9221@msn.com>
2022-11-21 14:11:34 -0800

    added BinaryPyramid101.js

---
+++ BinaryPyramid101.js
@@ -0,0 +1,26 @@
+// Binary Pyramid 101
+
+// Given two numbers m and n, such that 0 ≤ m ≤ n :
+
+// convert all numbers from m to n (inclusive) to binary
+// sum them as if they were in base 10
+// convert the result to binary
+// return as a string
+// Example
+// 1, 4  -->  1111010
+
+// because:
+//     1  // 1 in binary is 1
+// +  10  // 2 in binary is 10
+// +  11  // 3 in binary is 11
+// + 100  // 4 in binary is 100
+// -----
+//   122  // 122 in binary is 1111010
+
+function binaryPyramid(m,n) {
+    let arr = [];
+    for(let i = m; i <= n; i++) {
+      arr.push(i.toString(2));
+    }
+    return arr.reduce((a,c) => a + +c ,0).toString(2);
+}
