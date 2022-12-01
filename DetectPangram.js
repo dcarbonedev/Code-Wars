4835cda818509cd51353d858346ddfde67e2c72b
Daniel <dan9221@msn.com>
2022-12-01 11:50:03 -0800

    updated DetectPangram.js

--- DetectPangram.js
+++ DetectPangram.js
@@ -7,7 +7,27 @@
 // Given a string, detect whether or not it is a pangram. Return True if it is, False 
 // if not. Ignore numbers and punctuation.
 
+function isPangram(string){
+  //Parameters: String
+  //Return: Boolean (True if string is panagram, False if not)
+  //Examples Below (test cases)
 
+  //Pseudocode
+  //split string into array of lower case letters
+  let letters = string.toLowerCase().split('');
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  
+  //Loop through letter array to see if every letter of the alphabet is in the sentence
+  for(let i = 0; i < alphabet.length; i++) {
+    //If a letter is not present in the letters array then return false
+    if(!letters.includes(alphabet[i])) {
+      return false;
+    }
+  }
+  
+  //Return true (if all letters were found)
+  return true;
+}
   
   //Examples:
   console.log(isPangram("The quick brown fox jumps over the lazy dog."), true);
