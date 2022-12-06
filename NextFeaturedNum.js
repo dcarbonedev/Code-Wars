385ebdaf8f428e82bbb4d789bcadffa31b12a308
Daniel <dan9221@msn.com>
2022-12-06 14:08:40 -0800

    added NextFeaturedNum.js

---
+++ NextFeaturedNum.js
@@ -0,0 +1,21 @@
+// Next Featured Number Higher than a Given Value
+
+// Make a function that receives a value, val and outputs the smallest higher number than the given value, and this number belong to a set of positive integers that have the following properties:
+
+// their digits occur only once
+
+// they are odd
+
+// they are multiple of three
+
+// nextNumb(12) == 15
+
+// nextNumb(13) == 15
+
+// nextNumb(99) == 105
+
+// nextNumb(999999) == 1023459
+
+// nextNumber(9999999999) == "There is no possible number that
+// fulfills those requirements"
+
