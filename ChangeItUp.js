02b61f62882820050e808514e615cf91ba393ba7
Daniel <dan9221@msn.com>
2023-01-19 12:59:57 -0800

    modified ChangeItUp.js

--- ChangeItUp.js
+++ ChangeItUp.js
@@ -9,3 +9,13 @@
 // in this kata, y isn't considered as a vowel.
 // So, for example the string "Cat30" would return "dbU30" (Cat30 --> Dbu30 --> dbU30)
 
+function changer(str) {
+    let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let vowels = 'AEIOUaeiou';
+    let cons = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz';
+    str = str.split('').map(e => alpha.includes(e) ? 
+      alpha[alpha.indexOf(e) === 51 ? 26 : alpha.indexOf(e) === 25 ? 0 : alpha.indexOf(e)+1] : e);
+    str = str.map(e => vowels.includes(e) ? e.toUpperCase() : e);
+    str = str.map(e => cons.includes(e) ? e.toLowerCase() : e);
+    return str.join('');
+}
