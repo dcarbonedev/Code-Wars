01128bc3ea30f74b117c092e76e3e74af5133df3
Daniel <dan9221@msn.com>
2023-02-04 13:48:00 -0800

    updated CollatzLength.js

--- CollatzLength.js
+++ CollatzLength.js
@@ -13,3 +13,11 @@
 
 // For more reading see: http://en.wikipedia.org/wiki/Collatz_conjecture
 
+function collatz(n) {
+  let len = 0;
+  while(n > 1) {
+    n = n % 2 ? n * 3 + 1 : n / 2;
+    len++;
+  }
+  return len + 1;
+}
