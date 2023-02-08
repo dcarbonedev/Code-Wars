8ef40f4b601b3a110f562ae8c368258855504b77
Daniel <dan9221@msn.com>
2023-02-08 13:44:42 -0800

    updated PascalsTriangle2.js

--- PascalsTriangle2.js
+++ PascalsTriangle2.js
@@ -11,3 +11,16 @@
 //   1   2   1
 // 1   3   3   1
 
+function pascal(depth) {
+  let result = [[1]];
+  for(let i = 1; i < depth; i++) {
+    let temp = [];
+    temp.push(1);
+    for(let j = 0; j < result[i-1].length-1; j++) {
+      temp.push(result[i-1][j] + result[i-1][j+1]);
+    }
+    temp.push(1);
+    result.push(temp);
+  }
+  return result;
+}
