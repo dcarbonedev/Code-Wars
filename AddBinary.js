237d171d2a5317babbba59f1141d7bc95def29e2
Daniel <dan9221@msn.com>
2023-02-14 13:17:54 -0800

    added AddBinary.js

---
+++ AddBinary.js
@@ -0,0 +1,57 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+// Example 1:
+
+// Input: a = "11", b = "1"
+// Output: "100"
+// Example 2:
+
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+ 
+
+// Constraints:
+
+// 1 <= a.length, b.length <= 104
+// a and b consist only of '0' or '1' characters.
+// Each string does not contain leading zeros except for the zero itself.
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let result = '';
+    let carry = 0;
+    if(a.length > b.length) {
+        b = '0'.repeat(a.length - b.length) + b;
+    }
+    if(b.length > a.length) {
+        a = '0'.repeat(b.length - a.length) + a;
+    }
+
+    for(let i = a.length-1; i >= 0; i--) {
+        let total = +a[i] + +b[i] + carry;
+       if(total === 0) {
+         result += '0';
+         carry = 0;
+       }
+       else if(total === 1) {
+         result += '1';
+         carry = 0;
+       }
+       else if(total === 2) {
+         result += '0';
+         carry = 1;
+       }
+       else if(total === 3) {
+         result += '1';
+         carry = 1;
+       }
+    }
+    result = carry ? result + '1' : result;
+    return result.split('').reverse().join('');
+};
