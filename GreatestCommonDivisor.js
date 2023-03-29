f87f4ca73eb42c05a4ed470bc3470e6b7da342a9
Daniel <dan9221@msn.com>
2023-03-29 13:13:31 -0700

    updated GreatestCommonDivisor.js

--- GreatestCommonDivisor.js
+++ GreatestCommonDivisor.js
@@ -2,3 +2,12 @@
 
 // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 
+function mygcd(x,y) {
+  console.log('x:', x, 'y:', y);
+  for(let i = Math.min(x,y); i > 0; i--) {
+    if(x % i === 0 && y % i === 0) {
+      return i;
+    }
+  }
+}
+
