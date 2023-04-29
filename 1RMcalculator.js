ddb77c5a1a0e3951644dc312e2911c2f000b5155
Daniel <dan9221@msn.com>
2023-04-28 17:25:27 -0700

    updated 1RMcalculator.js

--- 1RMcalculator.js
+++ 1RMcalculator.js
@@ -40,3 +40,4 @@
  
 // Your function will receive a weight w and a number of repetitions r and must return your projected one repetition maximum. Since you are not sure which formula to use and you are feeling confident, your function will return the largest value from the three formulas shown above, rounded to the nearest integer. However, if the number of repetitions passed in is 1 (i.e., it is already a one rep max), your function must return w. Also, if the number of repetitions passed in is 0 (i.e., no repetitions were completed), your function must return 0.
 
+const calculate1RM = (w, r) => r === 1 ? w : r === 0 ? 0 : Math.max(Math.round(w * (1 + r/30)), Math.round((w*100) / (101.3 - 2.67123*r)), Math.round(w * (r**0.1))); 
