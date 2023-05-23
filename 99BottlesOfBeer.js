faf6ab9f33d026706cf2df4480a47ff971e617fd
Daniel <dan9221@msn.com>
2023-05-23 13:30:56 -0700

    updated 99BottlesOfBeer.js

--- 99BottlesOfBeer.js
+++ 99BottlesOfBeer.js
@@ -40,3 +40,15 @@
 //   "No more bottles of beer on the wall, no more bottles of beer.",
 //   "Go to the store and buy some more, 99 bottles of beer on the wall." ]
 
+function sing() {
+  let result = [];
+  for(let i = 99; i > 1; i--) {
+    result.push(i +' bottles of beer on the wall, ' +i +' bottles of beer.');
+    result.push('Take one down and pass it around, '  +(i-1) +' bottle' +(i === 2 ? '' : 's') +' of beer on the wall.');
+  }
+  result.push('1 bottle of beer on the wall, 1 bottle of beer.');
+  result.push('Take one down and pass it around, no more bottles of beer on the wall.');
+  result.push('No more bottles of beer on the wall, no more bottles of beer.');
+  result.push('Go to the store and buy some more, 99 bottles of beer on the wall.');
+  return result;
+}
