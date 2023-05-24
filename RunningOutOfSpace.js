64812138ee1cb01af0271f5ee9c8a1498ce00998
Daniel <dan9221@msn.com>
2023-05-24 12:28:53 -0700

    updated RunningOutOfSpace.js

--- RunningOutOfSpace.js
+++ RunningOutOfSpace.js
@@ -1,3 +1,11 @@
 // Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
 // For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
 
+function spacey(array) {
+  let result = [];
+  for(let i = 1; i < array.length+1; i++) {
+    result.push(array.slice(0, i).join(''));
+  }
+  return result;
+}
+
