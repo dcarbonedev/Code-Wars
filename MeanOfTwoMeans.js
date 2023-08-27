a9c7a13c8354e925f4c7f16fcce2f390aa30e72b
Daniel <dan9221@msn.com>
2023-08-27 13:28:48 -0700

    updated MeanOfTwoMeans.js

--- MeanOfTwoMeans.js
+++ MeanOfTwoMeans.js
@@ -12,3 +12,9 @@
 // [1, 3, 2, 4], 2, 8 => should return -1
 // because 8 is higher than the array's length.
 
+function getMean(arr, x, y){
+    if(x < 2 || y < 2 || x > arr.length || y > arr.length) return -1;
+    let mean1 = arr.slice(0,x).reduce((a,c) => a + c, 0) / x;
+    let mean2 = arr.slice(-y).reduce((a,c) => a + c, 0) / y;
+    return (mean1 + mean2) / 2;
+}
