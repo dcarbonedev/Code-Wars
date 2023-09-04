6e0d4df558e9e627e2134b8a725ebc1367850656
Daniel <dan9221@msn.com>
2023-09-04 15:45:14 -0700

    updated TwoNumsArePositive.js

--- TwoNumsArePositive.js
+++ TwoNumsArePositive.js
@@ -9,3 +9,8 @@
 // twoArePositive(4, 6, 10) == false
 // twoArePositive(-14, -3, -4) == false
 
+function twoArePositive(a, b, c) {
+    return a > 0 && b > 0 && c <= 0 ||
+           a > 0 && b <= 0 && c > 0 ||
+           a <= 0 && b > 0 && c > 0;
+}
