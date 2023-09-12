b237d6bc156498a4c05095d8256536a0331bd26c
Daniel <dan9221@msn.com>
2023-09-12 13:56:06 -0700

    updated FilterTheNumber.js

--- FilterTheNumber.js
+++ FilterTheNumber.js
@@ -7,3 +7,4 @@
 // Details
 // You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
 
+const filterString = s => +[...s].filter(e => '0123456789'.includes(e)).join('');
