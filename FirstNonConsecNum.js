793d123d81e6d31597c516d6ecaf25f507b8df30
Daniel <dan9221@msn.com>
2023-10-17 12:45:33 -0700

    updated FirstNonConsecNum.js

--- FirstNonConsecNum.js
+++ FirstNonConsecNum.js
@@ -8,3 +8,9 @@
 
 // The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
 
+function firstNonConsecutive(arr) {
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] - arr[i-1] > 1) return arr[i];
+  }
+  return null;
+}
