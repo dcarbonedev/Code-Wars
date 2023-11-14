3431552b47ccafe8efaf3a6ca6ffb53987ed8410
Daniel <dan9221@msn.com>
2023-11-14 11:39:02 -0800

    updated FindDupe.js

--- FindDupe.js
+++ FindDupe.js
@@ -6,3 +6,10 @@
 
 // You should return the duplicate value as a single integer.
 
+// loop
+function findDup(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) return arr[i];
+  }
+}
+
